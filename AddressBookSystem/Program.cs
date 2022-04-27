4e519b5fbc905e11239050a857dcfbcf77573726
Snehal1499 <snehalpatil1718@gmail.com>
2022-04-27 18:17:18 +0530

    Added UC1 AND UC2

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 
 namespace AddressBookSystem
 {
@@ -7,6 +8,16 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Address Book Problem");
+            //UC 1 and 2 Create Address Book DataTable
+            DataTable addressBook = new DataTable();
+            addressBook.TableName = "AddressBook";
+            addressBook.Columns.Add("First Name");
+            addressBook.Columns.Add("Last Name");
+            addressBook.Columns.Add("Address");
+            addressBook.Columns.Add("City");
+            addressBook.Columns.Add("State");
+            addressBook.Columns.Add("Phone Number");
+            addressBook.Columns.Add("Email");
         }
     }
 }
